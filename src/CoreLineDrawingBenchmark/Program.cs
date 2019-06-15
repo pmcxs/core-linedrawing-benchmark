9c4417fd64e51654b32590b9c34844ceaaf04618
Pedro Sousa <pedro.sousa1@gmail.com>
2019-06-15 16:06:28 +0100

    Main Logic

---
+++ src/CoreLineDrawingBenchmark/Program.cs
@@ -0,0 +1,109 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.Drawing;
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing.Drawing2D;
+using ImageSharpCustomDrawing;
+
+namespace CoreLineDrawingBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawLinesAndMeasure(imageSize: 1000, numberOfLines: 10, lineWidth: 1);
+            DrawLinesAndMeasure(imageSize: 1000, numberOfLines: 100, lineWidth: 1);
+            DrawLinesAndMeasure(imageSize: 1000, numberOfLines: 1000, lineWidth: 1);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:10, lineWidth:5);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:100, lineWidth:5);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:1000, lineWidth:5);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:10, lineWidth:10);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:100, lineWidth:10);
+            DrawLinesAndMeasure(imageSize:1000, numberOfLines:1000, lineWidth:10);
+        }
+
+        private static void DrawLinesAndMeasure(int imageSize, int numberOfLines, int lineWidth)
+        {
+            var lines = new List<int[]>();
+            var rnd = new Random(1);
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int pixelX = rnd.Next(imageSize);
+                int pixelY = rnd.Next(imageSize);
+                lines.Add(new int[] { pixelX, pixelY });
+            }
+
+            SixLabors.Primitives.PointF[] imageSharpLines = lines
+                .Select(r => new SixLabors.Primitives.PointF(r[0], r[1]))
+                .ToArray();
+
+            PointF[] systemDrawingLines = lines.Select(r => new PointF(r[0], r[1])).ToArray();
+
+            var w = new Stopwatch();
+
+            w.Start();
+            using (var image = new Image<Rgba32>(imageSize, imageSize))
+            {
+                image.Mutate(x => x
+
+                    .DrawLines(
+                        Rgba32.Red,
+                        lineWidth,
+                        imageSharpLines));
+
+                using (var stream = File.Create($"test-imageSharp-{imageSize}-{numberOfLines}-{lineWidth}.png"))
+                {
+                    image.SaveAsPng(stream);
+                }
+            }
+            w.Stop();
+
+            Console.WriteLine($"Took {w.ElapsedMilliseconds} ms with ImageSharp");
+
+            w.Restart();
+            using (var image = new Image<Rgba32>(imageSize, imageSize))
+            {
+                image.DrawLines(imageSharpLines, Rgba32.Red, lineWidth);
+
+                using (var stream = File.Create($"test-custom-{imageSize}-{numberOfLines}-{lineWidth}.png"))
+                {
+                    image.SaveAsPng(stream);
+                }
+            }
+            w.Stop();
+
+            Console.WriteLine($"Took {w.ElapsedMilliseconds} ms with Custom");
+
+            w.Restart();
+
+            using (var destination = new Bitmap(imageSize, imageSize))
+            using (var graphics = Graphics.FromImage(destination))
+            {
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+                using (var pen = new System.Drawing.Pen(System.Drawing.Color.Red, lineWidth))
+                {
+                    pen.StartCap = LineCap.Flat;
+                    pen.EndCap = LineCap.Flat;
+
+                    graphics.DrawLines(pen, systemDrawingLines);
+                }
+
+                using (var stream = File.Create($"test-systemDrawing-{imageSize}-{numberOfLines}-{lineWidth}.png"))
+                {
+                    destination.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                }
+
+                w.Stop();
+
+                Console.WriteLine($"Took {w.ElapsedMilliseconds} ms with System.Drawing.Common");
+            }
+        }
+    }
+}
---
+++ src/ImageSharpCustomDrawing/BresenhamLineAlgorithm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.Primitives;
+
+namespace ImageSharpCustomDrawing
+{
+    public static class BresenhamLineAlgorithm
+    {
+        /// <summary>
+        /// Draws a line between two points using Bresenham's algorithm with a thickness of 1 pixel
+        /// </summary>
+        /// <remarks>Algorithm from http://tech-algorithm.com/articles/drawing-line-using-bresenham-algorithm/</remarks>
+        /// <param name="image"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <param name="color"></param>
+        public static void DrawLine(Span<Rgba32> image, float x1, float y1, float x2, float y2, Rgba32 color, int width, int height)
+        {
+            float w = x2 - x1;
+            float h = y2 - y1;
+            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+            float longest = Math.Abs(w);
+            float shortest = Math.Abs(h);
+            if (!(longest > shortest))
+            {
+                longest = Math.Abs(h);
+                shortest = Math.Abs(w);
+                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+                dx2 = 0;
+            }
+            float numerator = (int)longest >> 1;
+            for (int i = 0; i <= longest; i++)
+            {
+                DrawingUtils.SetPixel(image, x1, y1, color, width, height);
+
+                numerator += shortest;
+                if (!(numerator < longest))
+                {
+                    numerator -= longest;
+                    x1 += dx1;
+                    y1 += dy1;
+                }
+                else
+                {
+                    x1 += dx2;
+                    y1 += dy2;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Using a variant of Bresenham algorithm obtains the various points of a line (without the first one) without any diagonal movement
+        /// </summary>
+        /// <param name="x0"></param>
+        /// <param name="y0"></param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="maxDistance"></param>
+        /// <returns></returns>
+        public static IEnumerable<Point> GetNonDiagonalPointsOfLine(int x0, int y0, Vector2 normalVector, float targetDistance)
+        {
+            int originalX = x0;
+            int originalY = y0;
+
+            int x1 = (int)Math.Round(x0 + normalVector.X);
+            int y1 = (int)Math.Round(y0 + normalVector.Y);
+
+            int xDist = Math.Abs(x1 - x0);
+            int yDist = -Math.Abs(y1 - y0);
+            int xStep = x0 < x1 ? +1 : -1;
+            int yStep = y0 < y1 ? +1 : -1;
+            int error = xDist + yDist;
+
+            //Add additional pixels while the distance is below the target one
+            while (Math.Sqrt(Math.Pow(x0 - originalX, 2) + Math.Pow(y0 - originalY, 2)) < targetDistance)
+            {
+                float e2 = error << 1;
+
+                if (e2 - yDist > xDist - e2)
+                {
+                    // horizontal step
+                    error += yDist;
+                    x0 += xStep;
+                }
+                else
+                {
+                    // vertical step
+                    error += xDist;
+                    y0 += yStep;
+                }
+
+                yield return new Point(x0, y0);
+            }
+        }
+    }
+
+}
